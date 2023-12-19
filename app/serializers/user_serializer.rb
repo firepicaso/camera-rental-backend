f20a486265d666ea140cea60139731b6be9a4a14
coker2019 <olabisi2k11@gmail.com>
2023-12-19 15:34:34 +0100

    update username

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :id, :email, :created_at
+  attributes :id, :username, :email, :created_at
 
   attribute :created_date do |user|
     user.created_at&.strftime('%m/%d/%Y')
