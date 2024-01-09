0c27327e10517601081413a87081577680e95885
coker2019 <olabisi2k11@gmail.com>
2024-01-09 18:01:59 +0100

    update db file

--- db/schema.rb
+++ db/schema.rb
@@ -50,12 +50,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_08_182619) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "jti", null: false
-    t.string "email"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
